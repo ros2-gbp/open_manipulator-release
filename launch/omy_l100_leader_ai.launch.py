63040a40a2039904793bea9e8cf1b8f0194bc8ec
Pyo <pyo@robotis.com>
2025-06-20 10:43:54 +0900

    Rebase from 'release/jazzy/open_manipulator_bringup'

--- launch/omy_l100_leader_ai.launch.py
+++ launch/omy_l100_leader_ai.launch.py
@@ -14,28 +14,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Author: Wonho Yoon, Sungho Woo
+# Author: Wonho Yun, Sungho Woo, Woojin Wie
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import RegisterEventHandler
-from launch.conditions import IfCondition
-from launch.conditions import UnlessCondition
-from launch.event_handlers import OnProcessExit
+from launch.actions import GroupAction
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.substitutions import FindExecutable
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     # Declare launch arguments
     declared_arguments = [
-        DeclareLaunchArgument(
-            'start_rviz', default_value='false', description='Whether to execute rviz2'
-        ),
         DeclareLaunchArgument(
             'prefix',
             default_value='""',
@@ -57,19 +55,24 @@ def generate_launch_description():
             description='Enable fake sensor commands.',
         ),
         DeclareLaunchArgument(
-            'run_init_position',
+            'port_name',
+            default_value='/dev/ttyUSB0',
+            description='Port name for hardware connection.',
+        ),
+        DeclareLaunchArgument(
+            'use_self_collision_avoidance',
             default_value='false',
-            description='Run init_position.py after launch',
+            description='Whether to launch the self-collision detection node',
         ),
     ]
 
     # Launch configurations
-    start_rviz = LaunchConfiguration('start_rviz')
     prefix = LaunchConfiguration('prefix')
+    use_self_collision_avoidance = LaunchConfiguration('use_self_collision_avoidance')
     use_sim = LaunchConfiguration('use_sim')
     use_fake_hardware = LaunchConfiguration('use_fake_hardware')
     fake_sensor_commands = LaunchConfiguration('fake_sensor_commands')
-    # run_init_position = LaunchConfiguration('run_init_position')
+    port_name = LaunchConfiguration('port_name')
 
     # Generate URDF file using xacro
     urdf_file = Command([
@@ -78,8 +81,8 @@ def generate_launch_description():
         PathJoinSubstitution([
             FindPackageShare('open_manipulator_description'),
             'urdf',
-            'om_y_follower',
-            'open_manipulator_y_follower.urdf.xacro',
+            'omy_l100',
+            'omy_l100.urdf.xacro',
         ]),
         ' ',
         'prefix:=',
@@ -93,87 +96,65 @@ def generate_launch_description():
         ' ',
         'fake_sensor_commands:=',
         fake_sensor_commands,
+        ' ',
+        'port_name:=',
+        port_name,
     ])
 
     # Paths for configuration files
     controller_manager_config = PathJoinSubstitution([
         FindPackageShare('open_manipulator_bringup'),
         'config',
-        'om_y_follower',
+        'omy_l100_leader_ai',
         'hardware_controller_manager.yaml',
     ])
-    rviz_config_file = PathJoinSubstitution([
-        FindPackageShare('open_manipulator_description'),
-        'rviz',
-        'open_manipulator.rviz',
-    ])
 
-    # Define nodes
     control_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
         parameters=[{'robot_description': urdf_file}, controller_manager_config],
         output='both',
         condition=UnlessCondition(use_sim),
-        remappings=[('/arm_controller/joint_trajectory', '/leader/joint_trajectory')],
     )
 
-    robot_state_pub_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': urdf_file, 'use_sim_time': use_sim}],
-        output='screen',
-    )
-
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', rviz_config_file],
-        output='screen',
-        condition=IfCondition(start_rviz),
-    )
-
-    joint_state_broadcaster_spawner = Node(
+    robot_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[
+            'gravity_compensation_controller',
+            'spring_actuator_controller',
             'joint_state_broadcaster',
-            '--controller-manager',
-            '/controller_manager',
+            'joint_trajectory_command_broadcaster',
         ],
-        output='screen',
+        parameters=[{'robot_description': urdf_file}],
     )
 
-    arm_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['arm_controller'],
-        output='screen',
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': urdf_file,
+                     'use_sim_time': use_sim,
+                     'frame_prefix': 'leader_'}],
+        output='both',
     )
 
-    # Event handlers to ensure order of execution
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner, on_exit=[rviz_node]
-        )
+    # Conditionally included self-collision detection launch
+    self_collision_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution([
+            FindPackageShare('open_manipulator_collision'),
+            'self_collision.launch.py'
+        ])),
+        condition=IfCondition(use_self_collision_avoidance)
     )
 
-    delay_arm_controller_spawner_after_joint_state_broadcaster_spawner = (
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster_spawner,
-                on_exit=[arm_controller_spawner],
-            )
-        )
-    )
-
-    return LaunchDescription(
-        declared_arguments
-        + [
+    leader_with_namespace = GroupAction(
+        actions=[
+            PushRosNamespace('leader'),
             control_node,
-            robot_state_pub_node,
-            joint_state_broadcaster_spawner,
-            delay_rviz_after_joint_state_broadcaster_spawner,
-            delay_arm_controller_spawner_after_joint_state_broadcaster_spawner,
+            robot_controller_spawner,
+            robot_state_publisher_node,
+            self_collision_launch,
         ]
     )
+
+    return LaunchDescription(declared_arguments + [leader_with_namespace])
