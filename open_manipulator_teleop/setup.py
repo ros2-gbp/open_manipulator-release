637e3bd7d8700dd22077dfe002afb2cad05c9c63
Pyo <pyo@robotis.com>
2025-10-27 17:57:26 +0900

    Revert "Trimmed the branch to only the open_manipulator_collision sub directory"
    This reverts commit 3c14d180f6d9581ac66c1ff92ed9f2ae91a6086e.

---
+++ open_manipulator_teleop/setup.py
@@ -0,0 +1,34 @@
+from glob import glob
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'open_manipulator_teleop'
+
+setup(
+    name=package_name,
+    version='4.0.8',
+    packages=find_packages(exclude=[]),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Pyo',
+    maintainer_email='pyo@robotis.com',
+    author='Sungho Woo, Wonho Yun',
+    author_email='wsh@robotis.com, ywh@robotis.com',
+    description='OpenManipulator teleoperation package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'open_manipulator_x_teleop = open_manipulator_teleop.open_manipulator_x_teleop:main',
+            'omy_3m_teleop = open_manipulator_teleop.omy_3m_teleop:main',
+            'omy_f3m_teleop = open_manipulator_teleop.omy_f3m_teleop:main',
+        ],
+    },
+)
